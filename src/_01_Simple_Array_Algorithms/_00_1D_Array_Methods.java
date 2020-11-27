421bd5596422e89b67aa3277dad9f400c8e45867
DerekHSA <55466846+DerekHSA@users.noreply.github.com>
2020-11-27 15:45:40 -0800

    Done

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,22 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+int FA = 0;
+for (int i = 0; i < values.length; i++) {
+	FA+=values[i];
+}
+		return FA;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+double FA = 0;
+for (int i = 0; i < values.length; i++) {
+	FA+=values[i];
+}
+FA/=values.length;
+		return FA;
 	}
 	
 	
@@ -20,15 +27,35 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+	boolean FA = false;
+	for (int i = 0; i < array.length; i++) {
+		if(array[i]==value) {
+			FA=true;
+		}
+	}
+		return FA;
 	}
 	
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
+		int FA= 0;
+		boolean gateway = false;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]==value) {
+				if(gateway==false) {
+					FA=i;
+					gateway=true;
+				}
+				
+			}
+			if(gateway==false) {
+				FA= -1;
+			}
+		}
 		
-		return 0;
+		return FA;
 	}
 }
+
